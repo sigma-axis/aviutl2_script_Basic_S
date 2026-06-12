68b0488e90d471a9294cf1d1bc934f5d6e265b90
sigma-axis <132639613+sigma-axis@users.noreply.github.com>
2026-06-13 04:19:59 +0900

    Better shader for corner rounding.

--- script/effect/四隅丸め/carve.hlsl
+++ script/effect/四隅丸め/carve.hlsl
@@ -47,109 +47,100 @@ float circle_dist(float2 pt, float r)
 	return length(pt) - r;
 }
 
-float2 shape_circle(float2 pt, float2 sz, float pad)
+float shape_circle(float2 pt, float2 sz)
 {
-	float v;
-	[branch] if (sz.x == sz.y) v = circle_dist(pt, sz.x);
-	else v = ellipse_dist(pt, sz);
-	return v + float2(0, pad);
+	[branch] if (sz.x == sz.y) return circle_dist(pt, sz.x);
+	else return ellipse_dist(pt, sz);
 }
-float2 shape_rhombus(float2 pt, float2 sz, float pad)
+float shape_rhombus(float2 pt, float2 sz)
 {
 	static const float3 L = { 1, 1, -1 }; // 45deg-line.
 	const float3 pt1 = { pt, 1 }, sza = { sz, sz.x * sz.y };
-	const float v = dot(pt1, sza.yxz * L) / length(sza.yx * L.xy);
-	return v + float2(0, pad);
+	return dot(pt1, sza.yxz * L) / length(sza.yx * L.xy);
 }
-float2 shape_octagon_S(float2 pt, float2 sz, float pad)
+float shape_octagon_S(float2 pt, float2 sz)
 {
 	static const float3 L = { 0.9238795, 0.38268343, -0.9238795 };
 	const float3 pt1 = { pt, 1 }, sza = { sz, sz.x * sz.y };
-	const float v = max(
+	return max(
 		dot(pt1, sza.yxz * L) / length(sza.yx * L.xy),
 		dot(pt1.yxz, sza * L) / length(sza.xy * L.xy));
-	return v + float2(0, pad);
 }
-float2 shape_octagon_R(float2 pt, float2 sz, float pad)
+float shape_octagon_R(float2 pt, float2 sz)
 {
 	static const float3 L0 = { 1, 0, -1 },
 		L1 = { 0.70710677, 0.70710677, -1 }; // 45deg-line.
 	const float3 pt1 = { pt, 1 }, sza = { sz, sz.x * sz.y };
-	const float v = max(max(
+	return max(max(
 		dot(pt1, sza.yxz * L0) / length(sza.yx * L0.xy),
 		dot(pt1.yxz, sza * L0) / length(sza.xy * L0.xy)),
 		dot(pt1, sza.yxz * L1) / length(sza.yx * L1.xy));
-	return v + float2(0, pad);
 }
-float2 shape_dodecagon_S(float2 pt, float2 sz, float pad)
+float shape_dodecagon_S(float2 pt, float2 sz)
 {
 	static const float3
 		L1 = { 0.9659258, 0.25881904, -0.9659258 }, // 15deg-line.
 		L2 = { 0.70710677, 0.70710677, -0.9659258 }; // 45deg-line.
 	const float3 pt1 = { pt, 1 }, sza = { sz, sz.x * sz.y };
-	const float v = max(max(
+	return max(max(
 		dot(pt1, sza.yxz * L1) / length(sza.yx * L1.xy),
 		dot(pt1.yxz, sza * L1) / length(sza.xy * L1.xy)),
 		dot(pt1, sza.yxz * L2) / length(sza.yx * L2.xy));
-	return v + float2(0, pad);
 }
-float2 shape_dodecagon_R(float2 pt, float2 sz, float pad)
+float shape_dodecagon_R(float2 pt, float2 sz)
 {
 	static const float3 L0 = { 1, 0, -1 },
 		L1 = { 0.8660254, 0.5, -1 }; // 30deg-line.
 	const float3 pt1 = { pt, 1 }, sza = { sz, sz.x * sz.y };
-	const float v = max(max(
+	return max(max(
 		dot(pt1, sza.yxz * L0) / length(sza.yx * L0.xy),
 		dot(pt1.yxz, sza * L0) / length(sza.xy * L0.xy)), max(
 		dot(pt1, sza.yxz * L1) / length(sza.yx * L1.xy),
 		dot(pt1.yxz, sza * L1) / length(sza.xy * L1.xy)));
-	return v + float2(0, pad);
 }
-float2 shape_spike(float2 pt, float2 sz, float pad)
+float shape_spike(float2 pt, float2 sz)
 {
 	static const float3
 		L1 = { 0.9659258, 0.25881904, -0.9659258 }, // 15deg-line.
 		L2 = { -0.5, 0.8660254, 0 }; // 120deg-line.
 	const float3 pt1 = { pt, 1 }, sza = { sz, sz.x * sz.y };
-	const float v = max(max(
+	return max(max(
 		dot(pt1, sza.yxz * L1) / length(sza.yx * L1.xy),
 		dot(pt1.yxz, sza * L1) / length(sza.xy * L1.xy)), min(
 		dot(pt1, sza.yxz * L2) / length(sza.yx * L2.xy),
 		dot(pt1.yxz, sza * L2) / length(sza.xy * L2.xy)));
-	return v + float2(0, pad);
 }
 
-float2 carve_shape(float2 pt, float2 sz, int shape)
+float carve_shape(float2 pt, float2 sz, int shape)
 {
-	[branch] if (any(sz <= 0)) return -1;
+	[branch] if (any(sz <= 0)) return min(pt.x, pt.y);
 	else {
 		[branch] switch (shape) {
-		case 0: default: return shape_circle(pt, sz, line_width);
-		case 1: return -shape_circle(sz - pt, sz, -line_width);
-		case 2: return shape_rhombus(pt, sz, line_width);
-		case 3: return min(pt.x, pt.y) + float2(0, line_width);
-		case 4: return shape_octagon_S(pt, sz, line_width);
-		case 5: return -shape_octagon_R(sz - pt, sz, -line_width);
-		case 6: return -shape_octagon_S(sz - pt, sz, -line_width);
-		case 7: return shape_dodecagon_S(pt, sz, line_width);
-		case 8: return -shape_dodecagon_R(sz - pt, sz, -line_width);
-		case 9: return -shape_dodecagon_S(sz - pt, sz, -line_width);
-		case 10: return -shape_spike(sz - pt, sz, -line_width);
-		case 11: return shape_spike(pt, sz, line_width);
+		case 0: default: return -shape_circle(sz - pt, sz);
+		case 1: return shape_circle(pt, sz);
+		case 2: return -shape_rhombus(sz - pt, sz);
+		case 3: return -min(sz.x - pt.x, sz.y - pt.y);
+		case 4: return -shape_octagon_S(sz - pt, sz);
+		case 5: return shape_octagon_R(pt, sz);
+		case 6: return shape_octagon_S(pt, sz);
+		case 7: return -shape_dodecagon_S(sz - pt, sz);
+		case 8: return shape_dodecagon_R(pt, sz);
+		case 9: return shape_dodecagon_S(pt, sz);
+		case 10: return shape_spike(pt, sz);
+		case 11: return -shape_spike(sz - pt, sz);
 		}
 	}
 }
 float4 carve(float4 pos : SV_Position) : SV_Target
 {
-	float2 outer = min(pos.xy, size - pos.xy);
-	outer = float2(-1, line_width - min(outer.x, outer.y));
+	float dist = min(min(pos.x, size.x - pos.x), min(pos.y, size.y - pos.y));
 
-	static const float2 corners[4] = { { 0, 0 }, { 1, 0 }, { 1, 1 }, { 0, 1 } };
+	static const int2 corners[4] = { { 0, 0 }, { 1, 0 }, { 1, 1 }, { 0, 1 } };
 	for (int c = 0; c < 4; c++) {
 		const float3 szs = size_shape[c];
-		outer = max(outer, carve_shape(
-			szs.xy - abs(pos.xy - corners[c] * size), szs.xy, int(szs.z)));
+		dist = min(dist, carve_shape(
+			abs(pos.xy - corners[c] * size), szs.xy, int(szs.z)));
 	}
-	const float2 alpha = saturate(0.5 - outer);
+	const float2 alpha = saturate(0.5 + dist - float2(0, line_width));
 	return float4(0, 0, 0, alpha[0] * (1 - alpha_hole * alpha[1]));
 }
