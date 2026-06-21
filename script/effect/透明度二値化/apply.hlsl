36712935fb8ec391018ef35b5a0d6c0605461f2c
sigma-axis <132639613+sigma-axis@users.noreply.github.com>
2026-06-22 00:13:39 +0900

    Fixed missing `saturate`.

--- script/effect/透明度二値化/apply.hlsl
+++ script/effect/透明度二値化/apply.hlsl
@@ -6,6 +6,6 @@ static const float least_a = 1.0 / 8192;
 float4 apply(float4 pos : SV_Position) : SV_Target
 {
 	const float4 col = src.Load(int3(pos.xy, 0));
-	const float a = (col.a - alpha - least_a) / (buffer - least_a);
+	const float a = saturate((col.a - alpha - least_a) / (buffer - least_a));
 	return float4((col.a > 0 ?  a / col.a : 0) * col.rgb, a);
 }
